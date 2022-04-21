11b79b78b0d79e8534dfb26e33192aefa6e7298a
Ksenia <naomi355@yandex.ru>
2022-04-22 01:17:02 +0300

    dist

---
+++ js/chunk-2d0e95df.b2e8dca4.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0e95df"],{"8cdb":function(e,n,t){"use strict";t.r(n);var c=function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div",[e._v("404 - Not Found")])},u=[],a={name:"404"},o=a,d=t("2877"),r=Object(d["a"])(o,c,u,!1,null,"9ae541c4",null);n["default"]=r.exports}}]);
+//# sourceMappingURL=chunk-2d0e95df.b2e8dca4.js.map
